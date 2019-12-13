948d9213d28cd2484d8d1ca287eeb5437e7e1605
Jason Kiraly <jasonkiraly@MacBook-Pro.hitronhub.home>
2019-12-13 14:47:22 -0800

    Basic header for example.

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,22 @@
 // Dependencies
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-
+import { Switch, Route, Link } from "react-router-dom";
 // Route Components
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route exact path="/" render={() => <Home test="test" />} />
-      <Route exact path="/about" render={() => <About />} />
-    </Switch>
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </header>
+      <Switch>
+        <Route exact path="/" render={() => <Home test="test" />} />
+        <Route exact path="/about" render={() => <About />} />
+      </Switch>
+    </>
   );
 };
 
